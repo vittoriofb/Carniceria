f84cfbf35c82ed3afd822e875a18826fce2c1ade
vittoriofb <162908112+vittoriofb@users.noreply.github.com>
2025-08-12 18:00:14 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,7 @@
 import logging
 import re
 from printer import send_to_printer
-
-PRODUCTOS_DB = {
-    "pollo": 6.50,
-    "ternera": 12.00,
-    "cerdo": 8.00,
-    "cordero": 13.50,
-    "conejo": 7.50,
-    "costilla": 8.00
-}
+from data import PRODUCTOS_DB
 
 SESSIONS = {}
 
